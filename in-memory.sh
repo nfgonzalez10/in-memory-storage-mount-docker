a8fea9280601a170cfdb50bd89cc75ea39a9e35b
nfgonzalez10 <nfgonzalez10@outlook.com>
2020-04-09 19:40:32 -0500

    :wrench: It was added configuration in memory mount

--- in-memory.sh
+++ in-memory.sh
@@ -1,1 +1,6 @@
 docker run --rm --mount type=tmpfs,dst=/tmp --entrypoint mount alpine:latest -v
+
+docker run --rm \
+    --mount type=tmpfs,dst=/tmp,tmpfs-size=16k,tmpfs-mode=1770 \
+    --entrypoint mount \
+    alpine:latest -v
